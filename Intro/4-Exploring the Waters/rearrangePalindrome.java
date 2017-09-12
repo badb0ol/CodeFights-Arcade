bfc67a29a66ddb9ca2e0e4beb47c91f14cfd6e31
clementvenot <clementvenot@users.noreply.github.com>
2017-09-12 16:55:20 +0200

    finished Exploring the Waters

--- Intro/4-Exploring the Waters/rearrangePalindrome.java
+++ Intro/4-Exploring the Waters/rearrangePalindrome.java
@@ -2,5 +2,18 @@
 // palindrome.
 
 boolean palindromeRearranging(String inputString) {
-  
+    int[] AlphArr = new int[26];
+    int same = 0;
+    for(int i = 0; i < inputString.length(); i++) {
+        AlphArr[inputString.charAt(i) - 'a']++;
+    }
+
+    for(int i = 0; i < 26; i++) {
+        if(AlphArr[i] % 2 == 1)
+            same++;
+    }
+    if(same % 2 == inputString.length() % 2 && same < 2)
+        return true;
+    else
+        return false;
 }
